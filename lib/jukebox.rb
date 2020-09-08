f22c652688deb559072074bd659ac8191030bfad
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-08 20:17:43 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,11 @@
-# Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(number)
+  puts "Please enter a song name or number:"
+end
