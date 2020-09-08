e74226c2f0d714ae20b1d41ca6ce8ea3ef358f65
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-08 21:13:43 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -6,6 +20,25 @@ def help
   puts "- exit : exits this program"
 end
 
-def play(number)
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i.next}. #{song}"
+  end
+end
+
+def play(songs)
   puts "Please enter a song name or number:"
+  song_input = gets.strip
+  songs.each_with_index do |song, i|
+  if song_input == song || song_input.to_i == i.next
+    # binding.pry
+    puts "Playing #{song}"
+  else
+    puts "Invalid input, please try again"
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
 end
